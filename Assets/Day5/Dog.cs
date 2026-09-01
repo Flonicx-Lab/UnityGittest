5e111fef1d415ec64fe50c5f616df521ef1615b8
Flonicx-Lab <flonicx22@gmail.com>
2026-09-01 15:45:17 +0900

    라이더 git ignore
    커밋

--- Assets/Day5/Dog.cs
+++ Assets/Day5/Dog.cs
@@ -9,7 +9,7 @@ public class Dog : Animal
         _height = height;
         _weight = weight;
     }
-
+    // 테스트 주석
     // override를 쓰지 않으면 재정의가 아니라 메서드 쉐도잉(하이딩)하고 있는 것.
     // 재정의 문법은 override를 써야한다.
     // 재정의를 하면 가리키는 실제 타입과 상관없이 실제 타입의 메서드를 호출한다.
